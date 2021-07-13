40886657241d1fc8e225a3af4fc5517499049980
IoannisSina <sinaioannis@gmail.com>
2021-07-13 12:05:36 +0300

    Issue #16. User now can play the audio file in the project page

---
+++ label_buddy/static/js/project_page.js
@@ -0,0 +1,9 @@
+// var wavesurfer = WaveSurfer.create({
+//     container: null
+// });
+
+// wavesurfer.load('http://ia902606.us.archive.org/35/items/shortpoetry_047_librivox/song_cjrg_teasdale_64kb.mp3');
+
+// wavesurfer.on('ready', function () {
+//     wavesurfer.play();
+// });
