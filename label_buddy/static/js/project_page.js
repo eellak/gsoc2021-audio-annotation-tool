cd8ad34eb4f308f782830ab99b7b07ec1bba290e
IoannisSina <sinaioannis@gmail.com>
2021-08-08 02:33:01 +0300

    Finished export UI button

--- label_buddy/static/js/project_page.js
+++ label_buddy/static/js/project_page.js
@@ -1,3 +1,5 @@
+var selected_export_format = null;
+
 // fix filters after page reload
 function fixFilters() {
     var parameters = window.location.href.split('?')[1];
@@ -84,4 +86,49 @@ function checkExtension(filePath) {
     }
 }
 
-window.onload = fixFilters;
+// dix for export click
+function selectExportFormat(div) {
+
+    // execute only if div not already selected
+    if(selected_export_format != div) {
+        let export_div_json = document.getElementById('export_div_json');
+        let json_radio = document.getElementById('json_radio');
+        let export_div_csv = document.getElementById('export_div_csv');
+        let csv_radio = document.getElementById('csv_radio');
+
+        if(export_div_json == div) {
+
+            // select json div
+            selected_export_format = export_div_json;
+            export_div_json.style.backgroundColor = 'rgba(115, 223, 237, 0.3)';
+            export_div_json.style.cursor = 'default';
+            json_radio.checked = true;
+
+            // unselect csv div
+            export_div_csv.style.backgroundColor = 'white';
+            export_div_csv.style.cursor = 'pointer';
+            csv_radio.checked = false;
+        } else {
+
+            // select csv div
+            selected_export_format = export_div_csv;
+            export_div_csv.style.backgroundColor = 'rgba(115, 223, 237, 0.3)';
+            export_div_csv.style.cursor = 'default';
+            csv_radio.checked = true;
+
+            // unselect json div
+            export_div_json.style.backgroundColor = 'white';
+            export_div_json.style.cursor = 'pointer';
+            json_radio.checked = false;
+        }
+    }
+}
+
+// export annotations for project request
+function exportDataRequest() {
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    fixFilters();
+    selected_export_format = document.getElementById('export_div_json');
+});
