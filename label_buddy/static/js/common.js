8a4ccda8ea4ae77045c250db445cdded49b45a3a
Vaibhav234k <114278562+Vaibhav234k@users.noreply.github.com>
2023-03-30 12:29:40 +0530

    setTimeout function added for alert close

--- label_buddy/static/js/common.js
+++ label_buddy/static/js/common.js
@@ -26,3 +26,9 @@ document.addEventListener('DOMContentLoaded', function() {
         NProgress.start();
     });
 });
+
+window.setTimeout(function() {
+    $(".alert").fadeTo(500, 0).slideUp(500, function(){
+        $(this).remove();
+    });
+}, 4000);
